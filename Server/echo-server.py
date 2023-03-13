1aa474b4213ad5533d4926f9d740f31cbc83ab9b
Hakiraa <julian.hareng@gmail.com>
2023-03-13 11:09:23 +0100

    qwe

--- Server/echo-server.py
+++ Server/echo-server.py
@@ -6,7 +6,8 @@ app = Flask(__name__)  # Setup the flask app by creating an instance of Flask
 # When someone goes to / on the server, execute the following function
 @app.route('/')
 def home():
-    return 'Hello, World!'  # Return this message back to the browser
+    return app.se
+    # Return this message back to the browser
 
 
 # If the script that was run is this script (we have not been imported)
